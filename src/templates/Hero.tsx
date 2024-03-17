0a6ae969469d1e0d55944ba06de1aca57f44d073
maxHyeon <maxhyeon.park@gmail.com>
2024-03-17 16:38:33 +0900

    mod header

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link';
 
 import { Background } from '../background/Background';
-import { Button } from '../button/Button';
-import { HeroOneButton } from '../hero/HeroOneButton';
 import { Section } from '../layout/Section';
 import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { Logo } from './Logo';
@@ -12,31 +10,23 @@ const Hero = () => (
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
         <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            GitHub
-          </Link>
-        </li>
-        <li>
           <Link href="/">Sign in</Link>
         </li>
       </NavbarTwoColumns>
     </Section>
 
-    <Section yPadding="pt-20 pb-32">
-      <HeroOneButton
-        title={
-          <>
-            {'The best solution for\n'}
-            <span className="text-primary-500">Basketball Club management</span>
-          </>
-        }
-        // description="The easiest way to build a React landing page in seconds."
-        button={
-          <Link href="https://creativedesignsguru.com/category/nextjs/">
-            <Button xl>Download Your Free Theme</Button>
-          </Link>
-        }
-      />
+    <Section yPadding="pt-6 pb-20">
+      <header className="text-center">
+        <h1 className="whitespace-pre-line text-5xl font-bold leading-hero text-gray-900 pb-6">
+          {'The best solution for\n'}
+          <span className="text-primary-500">Basketball Club management</span>
+        </h1>
+        <Link href="/">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Sign up with e-mail
+          </button>
+        </Link>
+      </header>
     </Section>
   </Background>
 );
