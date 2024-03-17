8b21b6125d7b9c3ea5a49f011eaa7f0650593058
maxHyeon <maxhyeon.park@gmail.com>
2024-03-17 16:21:57 +0900

    fix footer

--- src/footer/CenteredFooter.tsx
+++ src/footer/CenteredFooter.tsx
@@ -1,7 +1,6 @@
 import type { ReactNode } from 'react';
 
 import { FooterCopyright } from './FooterCopyright';
-import { FooterIconList } from './FooterIconList';
 
 type ICenteredFooterProps = {
   logo: ReactNode;
@@ -12,16 +11,6 @@ type ICenteredFooterProps = {
 const CenteredFooter = (props: ICenteredFooterProps) => (
   <div className="text-center">
     {props.logo}
-
-    <nav>
-      <ul className="navbar mt-5 flex flex-row justify-center text-xl font-medium text-gray-800">
-        {props.children}
-      </ul>
-    </nav>
-
-    <div className="mt-8 flex justify-center">
-      <FooterIconList>{props.iconList}</FooterIconList>
-    </div>
 
     <div className="mt-8 text-sm">
       <FooterCopyright />
--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   site_name: 'Hoopers',
-  title: 'Nextjs Hoopers',
+  title: 'Hoopers',
   description: 'Hoopers code for your Nextjs Boilerplate with Tailwind CSS',
   locale: 'en',
 };
